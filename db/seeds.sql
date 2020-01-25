31a4b145e15dc849fd39c6fa5742a7c4d3116803
Carlos Castrillon <carloscastrillon1991@gmail.com>
2020-01-24 23:23:43 -0500

    adding folders

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Double Bacon Cheeseburger');
+
+INSERT INTO burgers (burger_name) VALUES ('Texas Cheese Burger');
+
+INSERT INTO burgers (burger_name) VALUES ('Blue Cheese  Burger');
